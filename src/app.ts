11aa815639f1949315cd416600920482e0d185be
Kevin O'Connell <kevinoconnell42@gmail.com>
2024-03-17 12:58:47 -0400

    ok

--- src/app.ts
+++ src/app.ts
@@ -44,7 +44,7 @@ if (STATSD_HOST) {
   initializeStatsd(STATSD_HOST, STATSD_METRICS_PREFIX);
 }
 
-const db = getDbClient(POSTGRES_URL);
+const db = getDbClient(process.env.POSTGRES_URL);
 onTerminate(async () => {
   log.debug("Disconnecting from database");
   await db.destroy();
